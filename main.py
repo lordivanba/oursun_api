f78caf5625dd7d614ab3408b41ac773e2a995c9f
Ivan Canto <ivanbamail@gmail.com>
2023-09-29 16:23:01 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/bambam")
+def bambam():
+    return {
+        "success": True,
+        "data": [
+            {
+                "user": "bambam",
+                "email": "hello@bambamcodes.com",
+                "createdAt": "29/09/2023",
+            }
+        ],
+        "pagination": {
+            "page": 1,
+            "total": 1,
+        },
+    }
+
+
+@app.get("/items/{item_id}")
+def read_item(
+    item_id: int,
+    q: Union[str, None] = None,
+):
+    return {
+        "item_id": item_id,
+        "q": q,
+    }
