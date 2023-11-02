793f24c6a005819be756a0cb4ba9eb040baa2350
Ivan Canto <ivanbamail@gmail.com>
2023-11-01 23:27:10 -0600

    I enabled cors

--- app/api.py
+++ app/api.py
@@ -1,10 +1,20 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from router import user
 
 from router import kits
 
 app = FastAPI()
+
+# Configure CORS settings to allow requests from any origin
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow requests from any origin
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/", tags=["Root"])
